3cb274610f5f0d7269c3b55a5cf6be726ab96ffa
hsia <hchsia@gmail.com>
2020-05-13 00:09:17 +0800

    change text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Hi Hi!
+          Feature branch!
         </a>
       </header>
     </div>
